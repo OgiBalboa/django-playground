f974a882ff067e26bde751bc1e5b6f76f3dccb4d
OgiBaloba <sismanogulcan@gmail.com>
2021-01-21 22:59:02 +0300

    "selection fields final and little changes on dynamic forms"

--- quas/static_in_env/js/modules/dynamic-forms.js
+++ quas/static_in_env/js/modules/dynamic-forms.js
@@ -7,28 +7,30 @@ multiple_script.type = 'text/html';
 multiple_script.src = "multiple-select.min.js"
 */
 var ROBOT_APPLICATIONS = ["Arc Welding", "Cleaning", "Coating", "Collaboration", "Cutting", "Deburring", "Depalletizing", "Die Casting", "Dispensing", "Enamelling", "Full Layer Palletizing", "Glazing", "Gluing", "Grinding", "Heavy Arc Welding", "Injection Moulding", "Item Picking", "Loading", "Loading and Unloading"," Machine Tending", "Machine Handling", "Measuring", "Packing", "Painting", "Palletizing", "Part Inspection", "Picking", "Polishing", "Powdering", "Powertrain Assembly", "Pre-Machining", "Press Automation", "Press Brake Tending", "Press Tending", "Rubber Insertion", "Screw Driving", "Sealing", "Small Parts Assembly", "Spot Welding", "Spraying", "Testing", "Unloading", ]
+var AXIS_NUMBER = [1,2,3,4,5,6];
+var MOUNTING_TYPES = ["Any", "Floor", "Wall", "Tilted", "Invert Mount",];
 var FIND_ROBOT_PARAMETER = {
     "reach":["float",["reach","Specify Reach","m"]],
     "payload":["float",["payload","Specify Payload","kg"]],
     "application":["multi-select",["application","Choose Application Type",ROBOT_APPLICATIONS]],
     "performance_rating":["int",["performance_rating","Specify Performance Rating Range"]],
     "customer_rating":["int",["customer_rating","Specify Customer Rating Range"]],
-    "axis_number":["multi-select",["axis_number","Specify Axis Number"]],
+    "axis_number":["multi-select",["axis_number","Specify Axis Number",AXIS_NUMBER]],
     "brand":["text",["brand","Specify Brand Name",false]],
     "repeatability":["float",["repeatability","Specify Repeatability","mm"]],
     "picking_cycle":["float",["picking_cycle","Specify Picking Cycle","s"]],
-    "mounting":["multi-select",["mounting","Specify Mounting Type"]],
+    "mounting":["multi-select",["mounting","Specify Mounting Type",MOUNTING_TYPES]],
     "weight":["int",["weight","Specify Weight","kg"]],
     "speed":["float",["speed","Specify Speed","m/s"]],
 }
 //TODO: parametreleri şu formatta düzenle: [id, [["text1,unit1], [text2,unit2]] ] böylece sınırsız text field açarsın.
 //----------------------FIELDS--------------------
-var FIELDS = [];
+var FIELDS = {};
 class StringField {
     constructor(id,text,unit) {
     this.div = document.createElement("div");
-    this.div.id = id +"_div"; //TODO:(LATER)WHAT IF THERE ARE MORE THAN ONE OF SAME FIELDS?
-    var id = this.div.id; //TODO:(learn) this değişkenleri alt fonksiyonlarda kullanılamıyor. bu sebeple var ile değişken atadık. Alternatif çözümü var mı?
+    this.id = id //TODO:(LATER)WHAT IF THERE ARE MORE THAN ONE OF SAME FIELDS?
+    var id = id; //TODO:(learn) this değişkenleri alt fonksiyonlarda kullanılamıyor. bu sebeple var ile değişken atadık. Alternatif çözümü var mı?
     this.div.className = "input-group mb-3";
     //---------------------------------------
     var inner_div = document.createElement("div");
@@ -41,7 +43,7 @@ class StringField {
     //---------------------------------------
     this.input = document.createElement("INPUT");
     this.input.setAttribute("type", "text");
-    this.input.id = id;
+    //this.input.id = id;
     this.input.className = "form-control";
     this.div.appendChild(this.input);
     if (unit != false){
@@ -54,7 +56,6 @@ class StringField {
     }
     this.remove_button = document.createElement("button");
     this.remove_button.setAttribute("style", "color:red;");
-    //this.remove_button.setAttribute("onclick","remove_field('${this.div.id}')");
     this.remove_button.addEventListener("click",function(){remove_field(id)});
     var img = document.createElement("i");
     img.className = "fas fa-trash float-center";
@@ -71,9 +72,12 @@ class StringField {
 class MultiSelectField {
     constructor(id,text,choices,values = choices) {
     this.selected = [];
+    this.on_selected = this.on_selected.bind(this);
     //-------------MAIN DIV-------------
     this.div = document.createElement("div");
     this.div.className = "col"
+    this.id = id;
+    var id = id;
     //-------------SELECT -------------
     this.select = document.createElement("select");
     this.select.id = id;
@@ -86,7 +90,7 @@ class MultiSelectField {
     this.default_option.style = "color:red"
     this.default_option.value = "default";
     this.default_option.textContent = text;
-    this.select.addEventListener("change", this.on_selected);
+    this.select.addEventListener("change",this.on_selected);
     this.select.appendChild(this.default_option);
     for (var i = 0; i <choices.length;i++){
         var option = document.createElement("option");
@@ -99,33 +103,45 @@ class MultiSelectField {
 
     }
     get_value(){
-      var result = [];
-      var options = this.select && this.select.options;
-      var opt;
-
-      for (var i=0; i<options.length; i++) {
-        opt = options[i];
-
-        if (opt.selected) {
-          result.push(opt.value || opt.text);
-        }
-      }
-      return result;
+      return this.selected;
     }
     on_selected(event) {
     var selectElement = event.target;
     var value = selectElement.value;
+    if (value == "default") return;
     if (this.selected.includes(value)) {
+        this.change_option_name(selectElement,value,value);
+        this.selected = this.selected.filter(function(opt){if (opt != value){return opt} });
+    }
+    else {
+        this.selected.push(value);
+        this.change_option_name(selectElement,value,value+" (selected)");
+    }
+    selectElement.selectedIndex = 0;
+     if (this.selected.length >0) {
+    var selected_names = ""
+    for (var i=0; i<this.selected.length; i++) {
+        selected_names += this.selected[i] + ", ";
+    }}
+    else selected_names = "Choose Application Type";
+    this.change_option_name(selectElement, "default", selected_names);
+    }
+    //-----------CHANGE OPTION NAME FUNCTION-----------
+    change_option_name(select,val,name){
+        for (var i=0; i<select.length; i++) {
+        if (select.options[i].value == val){
+            select.options[i].innerHTML = name;
+            }
+        }
+    }
 
-    }
-    this.selected.push(value);
-    }
     }
 class SingleSelectField {
     constructor(id, text, choices, values = choices){
     this.div = document.createElement("div");
     this.select = document.createElement("select");
-    this.select.id = id;
+    this.id = id;
+    this.id = id;
     this.select.className ="browser-default custom-select mb-3";
     this.select.addEventListener("change", add_field);
     var default_option = document.createElement("option");
@@ -145,6 +161,13 @@ class SingleSelectField {
 }
 //------------------FUNCTIONS----------------
 var FIELD_SELECT = "";
+var FORM_div = document.getElementById("findrobot_form_div");
+var FORM = document.getElementById("findrobot_form");
+var SUBMIT = document.createElement("button");
+SUBMIT.className = "btn btn-info btn-block unique-color my-4waves-effect active mb-4";
+SUBMIT.textContent = "Find BestRobot!";
+SUBMIT.addEventListener("click",pull_form);
+SUBMIT.type = "button";
 function field_selector() {
     parameters = Object.values(FIND_ROBOT_PARAMETER);
     var choices = [];
@@ -154,7 +177,8 @@ function field_selector() {
         values.push(parameters[i][1][0]);
     }
     FIELD_SELECT = new SingleSelectField("select_field","Choose a search parameter",choices,values);
-    document.getElementById("findrobot_form").appendChild(FIELD_SELECT.div);
+    FORM_div.appendChild(FIELD_SELECT.div);
+    FORM.appendChild(SUBMIT);
 }
 function add_field(event) {
 //TODO:add error handling here also parameters can be a class
@@ -162,8 +186,8 @@ function add_field(event) {
     var value = selectElement.value;
     var params = FIND_ROBOT_PARAMETER[value];
     FIELD_SELECT.select.selectedIndex = 0; // RESET SELECTOR
-    if (FIELDS.includes(params[1][0])) return; // DONT DUPLICATE FIELDS
-    FIELDS.push(params[1][0]);
+    // DONT DUPLICATE FIELDS
+    if (value in FIELDS) return;
     switch (params[0]) {
     case "int":
         field = new StringField(...params[1]);
@@ -181,52 +205,15 @@ function add_field(event) {
         field = new MultiSelectField(...params[1]);
         break;
     }
-    document.getElementById("findrobot_form").appendChild(field.div);
 
+    FIELDS[field.id] = field;
+    FORM_div.appendChild(field.div);
 }
 function remove_field(x){
 pull_form();
-document.getElementById("findrobot_form").removeChild(document.getElementById(x));
-FIELDS = FIELDS.filter(function(id){if (id != x) return id }); // REMOVE FIELD FROM LIST REGISTER
-console.log(FIELDS);
+FORM_div.removeChild(document.getElementById(x));
+delete FIELDS[x];
 }
-function create__form() {
-document.getElementById("findorobot_form").innerHTML =`
-        <!-- Form -->
-        <form class="text-center" style="color: #757575;" method="POST">
-            <select class="browser-default custom-select mb-3">
-              <option selected>Choose Application Type</option>
-                 <option value="aa">${a}</option>
-            </select>
-            <!-- Name -->
-            <div class="input-group mb-3" >
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">Specify Payload</span>
-                  </div>
-                <input type="text" id="payload" name=payload class="form-control">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">kg</span>
-                </div>
-            </div>
-            <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">Specify Reach</span>
-                  </div>
-                <input type="text" id="reach" name="reach" class="form-control">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">m</span>
-                </div>
-            </div>
-
-            <!-- Sign in button -->
-            <button class="btn btn-info btn-block my-4waves-effect" type="submit">Find BestRobot!</button>
-
-        </form>
-        <!-- Form -->
-`
-}
-
 function pull_form() {
-    //console.log(document.getElementById("application").value);
-    //field.div.appendChild(script);
+console.log(FIELDS["application"].get_value());
 }
